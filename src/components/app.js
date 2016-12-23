b5f4cbe837f7b30c578877bb40889468b5c6a9ce
Ilya Sharonov <ilia.sharonov@gmail.com>
2016-12-23 19:17:59 +0300

    Implemented initial components

---
+++ src/components/app.js
@@ -0,0 +1,25 @@
+import React, { Component, PropTypes } from 'react';
+import { Router, Route, browserHistory } from 'react-router';
+import { Provider } from 'react-redux';
+
+import Root from './root';
+
+class App extends Component {
+
+  render() {
+    return (
+      <Provider store={this.props.store}>
+        <Router history={browserHistory}>
+          <Route path="/" component={Root} />
+        </Router>
+      </Provider>
+    );
+  }
+
+}
+
+App.propTypes = {
+  store: PropTypes.object.isRequired,
+};
+
+export default App;
